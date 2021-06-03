a79ed561e74b1d857012ee795da412b4b4f4f9a1
shivammathur4 <35236428+shivammathur4@users.noreply.github.com>
2021-06-03 10:57:58 +0530

    Add files via upload
    Sequence practice Problems

---
+++ AddDiceNumbers.js
@@ -0,0 +1,5 @@
+let a= (Math.floor(Math.random() * 10) % 6) + 1;
+let b= (Math.floor(Math.random() * 10) % 6) + 1;
+let c=a+b;
+console.log("Number 1: " + a + " Number 2: " + b);
+console.log("Number Addition is: " + c);

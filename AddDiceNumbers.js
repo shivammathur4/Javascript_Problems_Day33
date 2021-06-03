f409a81179cf29ce7091b17e3bef9a1857d4cc05
shivammathur4 <35236428+shivammathur4@users.noreply.github.com>
2021-06-03 12:57:05 +0530

    Delete AddDiceNumbers.js

--- AddDiceNumbers.js
+++
@@ -1,5 +0,0 @@
-let a= (Math.floor(Math.random() * 10) % 6) + 1;
-let b= (Math.floor(Math.random() * 10) % 6) + 1;
-let c=a+b;
-console.log("Number 1: " + a + " Number 2: " + b);
-console.log("Number Addition is: " + c);
